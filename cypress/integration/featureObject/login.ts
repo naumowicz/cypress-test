5b6cc31dca178bfe50b1e205fc4a1fd499a1b72c
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-07-21 01:03:11 +0200

    login feature object

---
+++ cypress/integration/featureObject/login.ts
@@ -0,0 +1,11 @@
+import actions from '../../support/actions';
+
+class Login {
+	loginButton = 'app-button';
+
+	clickLogin() {
+		actions.click(this.loginButton);
+	}
+}
+
+export default new Login();
