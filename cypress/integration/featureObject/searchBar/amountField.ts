7838bc911033015381383d241961f327be89e7c9
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-07-21 00:51:28 +0200

    using find button

--- cypress/integration/featureObject/searchBar/amountField.ts
+++ cypress/integration/featureObject/searchBar/amountField.ts
@@ -4,11 +4,13 @@ class amoutField {
 	button = 'button[data-cy="search-amount-button"]';
 	from = 'input[placeholder="Od"]';
 	to = 'input[placeholder="Do"]';
+	find = '[role] > [type="button"]:nth-of-type(2)'
 	
 	setAmountRange(range1: string, range2: string) {
 		actions.click(this.button);
 		actions.type(this.from, range1);
 		actions.type(this.to, range2);
+		actions.click(this.find);
 	}
 }
 
