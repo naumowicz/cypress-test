db14d14ee2f78addd44032458945d800920cfaed
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-07-21 13:17:07 +0200

    test data for transactions

---
+++ cypress/testData/transactionsDataList.js
@@ -0,0 +1,43 @@
+const liftago = {
+	list: [
+		{
+			header: 'Únor 2016',
+			transactions: [
+				{
+					id: '100000448218838',
+					time: '25.02.2016',
+					title: 'Jiří Spokojený Liftago.com Usti nad Labe CZ',
+					category: 'Taxi',
+					paymentType: 'platba kartou',
+					amount: '-196,00 Kč',
+					euro: '€ 7,25',
+				},
+				{
+					id: '100000446866115',
+					time: '24.02.2016',
+					title: 'Jiří Spokojený Liftago.com Usti nad Labe CZ',
+					category: 'Taxi',
+					paymentType: 'platba kartou',
+					amount: '-200,00 Kč',
+					euro: '€ 7,40',
+				},
+			],
+		},
+		{
+			header: 'Leden 2016',
+			transactions: [
+				{
+					id: '100000390037039',
+					time: '30.01.2016',
+					name: 'Jiří Spokojený Liftago.com Usti nad Labe CZ',
+					category: 'Taxi',
+					paymentType: 'platba kartou',
+					amount: '-255,00 Kč',
+					euro: '€ 9,44',
+				},
+			],
+		},
+	],
+};
+
+export default { liftago: liftago };
